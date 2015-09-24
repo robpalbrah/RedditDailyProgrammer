deea433fedfa70c97feca7a2497e11c4480c7f77
robpalbrah <robpalbrah@gmail.com>
2015-09-24 08:08:04 +0400

    Works on Demetri Martin poem 'Dammit I'm mad'

--- easy/232.py
+++ easy/232.py
@@ -1,13 +1,19 @@
 # Reddit Daily Programming [2015-09-14] Challenge #232 [Easy] Palindromes
 # https://www.reddit.com/r/dailyprogrammer/comments/3kx6oh/20150914_challenge_232_easy_palindromes/
 
-lines_number = input("Number of lines: ")
+file = open(r'C:\code\rDP\DemetriMartin.txt', 'r')
 
 lines = []
-for i in range(int(lines_number)):
-    input_lines = input('> ').lower()
-    input_lines = input_lines.translate({ord(i): None for i in ' $&?.,#!;:-'})
+for file_line in file.readlines():
+    
+    table_string = u'\u2026\u201C\u201D\u2019 "$&?.,#!;:-()\'\"'
+    tr_table = {ord(i): None for i in table_string} 
+    
+    input_lines = file_line.lower().translate(tr_table)
+    
     lines.append(input_lines.rstrip())
+
+file.close()
     
 lines = ''.join(lines)
 lines_reversed = lines[::-1] 
