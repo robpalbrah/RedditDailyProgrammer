abb00ed11565cc03135ba5ae8d99ededa7cc597f
robpalbrah <robpalbrah@gmail.com>
2015-09-28 06:42:53 +0400

    rDP 231 Now it works!

--- easy/231.py
+++ easy/231.py
@@ -4,29 +4,42 @@
 
 # Rule 90 is a Cellular Automata rule based on the XOR function. 
 
-input_string = input('> ')
+def write_in_file(nextline, work_file):
+    """Takes a list, makes it a string and writes it into a file, 
+    substituting 1 for 'X' and 0 for whitespace."""
+    
+    nextline_string = ''.join(nextline) 
+    translate_table = {ord('1'): 'X', ord('0'): ' '}
+    work_file.write(nextline_string.translate(translate_table) + '\n')
 
-number_of_lines = 25 
+input_string = '00000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000'
+number_of_lines = 25
 
 work_list = [a for a in input_string] 
+work_file = open(r'c:\code\GitHub\rDP\Easy\231_output.txt', 'w')
+
+write_in_file(work_list, work_file)
 
 for n in range(number_of_lines):
     nextline = ['0' for i in range(len(work_list))] 
+    index = 0
+    
     for elem in work_list:
-        index = work_list.index(elem)
-        xor_rule = work_list[index - 1] != work_list[index + 1] 
         # Check if index a boundary element of the list
-        if index not in (0, len(work_list)):
+        if index not in (0, len(work_list) - 1): 
+            # (len(work_list) - 1) because indexing starts from 0
+            xor_rule = work_list[index - 1] != work_list[index + 1] 
+            
             if xor_rule: 
                 nextline[index] = '1'
             else:
                 nextline[index] = '0'
-                
+        index += 1
     work_list = nextline
     
-    nextline_string = ''.join(nextline)
-    nextline_string.translate({ord('1'): 'X'})
-    print(nextline_string)
-                
+    write_in_file(nextline, work_file)
+    
+work_file.close()
+          
                 
                 
