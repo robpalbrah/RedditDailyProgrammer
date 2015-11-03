faef55a2e191274ecfa70ceb5ffec0c4b4d1896d
robpalbrah <robpalbrah@gmail.com>
2015-11-03 07:34:20 +0400

    Done

--- easy/dp_218_easy.py
+++ easy/dp_218_easy.py
@@ -6,23 +6,30 @@ https://tinyurl.com/dp-218-easy
 def calculate_palindrome(number, STEPS = 10000):
     """Calculates palindromic number. Returns (palindrome, counter).
     Returns None if no palindrome exists for this number."""
-    counter = 0 
     
-    while counter < STEPS:
+    for i in range(STEPS):
         if number == number[::-1]:
-            return (number, counter) 
-    
-        counter += 1
+            return (number, i) 
         number = str(int(number) + int(number[::-1]))
-        
     else: 
         return None 
 
-input_number = input("Enter a number please.\n>")
-palindrome = calculate_palindrome(input_number)
 
-if palindrome:
-    print("%s gets palindromic after %s steps: %s" % (input_number, 
-          palindrome[1], palindrome[0]))
-else:
-    print("%s not a palindromic number" % input_number)
+palindromes = dict()
+lychrel_numbers = list()
+
+for number in range(1, 1001):
+    try:
+        palindrome = calculate_palindrome(str(number))[0]
+        print(number, palindrome) # print test
+    except TypeError: 
+        lychrel_numbers.append(number)
+        continue  
+    
+    if palindrome not in palindromes.keys():
+        palindromes[palindrome] = [] 
+    
+    palindromes[palindrome].append(number)
+   
+print(lychrel_numbers)
+    
