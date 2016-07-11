c8572cb8ba0a15870aff3db912a50af2d43f49b9
robpalbrah <robpalbrah@gmail.com>
2016-07-11 08:14:53 +0400

    dp_272_easy No edge cases

--- easy/dp_272_easy.py
+++ easy/dp_272_easy.py
@@ -15,61 +15,58 @@ class Scrubble:
                   blank tile in tile_bag = [[tile, count, value] ...] format.
     """
     def __init__(self):
-        self.tile_bag = [['_', 2, 0],
-                         ['A', 9, 1],
-                         ['B', 2, 3],
-                         ['C', 2, 3],
-                         ['D', 4, 2],
-                         ['E', 12, 1],
-                         ['F', 2, 4],
-                         ['G', 3, 2],
-                         ['H', 2, 4],
-                         ['I', 9, 1],
-                         ['J', 1, 8],
-                         ['K', 1, 5],
-                         ['L', 4, 1],
-                         ['M', 2, 3],
-                         ['N', 6, 1],
-                         ['O', 8, 1],
-                         ['P', 2, 3],
-                         ['Q', 1, 10],
-                         ['R', 6, 1],
-                         ['S', 4, 1],
-                         ['T', 6, 1],
-                         ['U', 4, 1],
-                         ['V', 2, 4],
-                         ['W', 2, 4],
-                         ['X', 1, 8],
-                         ['Y', 2, 4],
-                         ['Z', 1, 10]]
-        #  non-public attribute _positions added for the fast access
-        # to the tile_bag
-        self._positions = {tile: pos for (pos, (tile, _, _))
-                           in enumerate(self.tile_bag)}
+        # CHANGED from list of list to dictionary.
+        self.tile_bag = {'_': [2, 0],
+                         'A': [9, 1],
+                         'B': [2, 3],
+                         'C': [2, 3],
+                         'D': [4, 2],
+                         'E': [12, 1],
+                         'F': [2, 4],
+                         'G': [3, 2],
+                         'H': [2, 4],
+                         'I': [9, 1],
+                         'J': [1, 8],
+                         'K': [1, 5],
+                         'L': [4, 1],
+                         'M': [2, 3],
+                         'N': [6, 1],
+                         'O': [8, 1],
+                         'P': [2, 3],
+                         'Q': [1, 10],
+                         'R': [6, 1],
+                         'S': [4, 1],
+                         'T': [6, 1],
+                         'U': [4, 1],
+                         'V': [2, 4],
+                         'W': [2, 4],
+                         'X': [1, 8],
+                         'Y': [2, 4],
+                         'Z': [1, 10]}
         # non-public attribute _tiles_string used to create
         # random sets of tiles
         self._tiles_string = ""
-        for (tile, count, _) in self.tile_bag:
+        for (tile, (count, _)) in self.tile_bag.items():
             self._tiles_string += "".join(tile * count)
         # non-public attribute _sorted_bag created in _sort_bag method
         self._sorted_bag = None
 
     def _take_one_tile(self, tile):
         """Remove one tile from tile_bag.
-        
-        Decreases tile_count by one. Removes tile from _tiles_string. 
-        Prints error message if no requested tiles lift in tiles_bag. 
-        
+
+        Decreases tile_count by one. Removes tile from _tiles_string.
+        Prints error message if no requested tiles lift in tiles_bag.
+
         Args:
             tile (str): requested tile.
         Returns:
             removed (bool): True if tile were successfully removed from
-                tile_bag and _tiles_string; False otherwise.  
+                tile_bag and _tiles_string; False otherwise.
         """
         removed = False
-        tile_count = self.tile_bag[self._positions[tile]][1]
+        tile_count = self.tile_bag[tile][0]
         if tile_count > 0:
-            self.tile_bag[self._positions[tile]][1] -= 1
+            self.tile_bag[tile][0] -= 1
             self._tiles_string = self._tiles_string.replace(tile, "")
             removed = True
         else:
@@ -78,75 +75,68 @@ class Scrubble:
                   % tile)
         return removed
 
-    def _sort_bag(self):
-        """Sort tile_bag by the amount of tiles left in reverse order.
-        
-        """
-        self._sorted_bag = sorted(self.tile_bag,
-                                  key=lambda tile_info: tile_info[1],
-                                  reverse=True)
-
     def get_tiles_left(self):
-        """Get an ordered list of lists of tiles that are left in a bag.
+        """Get dict {amount_left: [tiles],...} from the tile_bag.
 
         Returns:
-            tiles_left (list): ordered list of lists in [[num_left, tile_1,
-                tile_2, ...], ...] format of tiles lift in tiles_bag.
+            tiles_left (dict): {count: [tiles], ...}.
         """
-        self._sort_bag()
-
-        tiles_left = []
-
-        num_tiles = self._sorted_bag[0][1]
-        tiles_left_row = [num_tiles]
-
-        # value in the tuple left unused to provide possibility
-        # for the future extension with addition of scores
-        for (tile, count, value) in self._sorted_bag:
-            if count == num_tiles:
-                tiles_left_row.append(tile)
-            elif count < num_tiles:
-                tiles_left.append(tiles_left_row)
-
-                num_tiles = count
-                tiles_left_row = [num_tiles]
-
-                tiles_left_row.append(tile)
-
-        tiles_left.append(tiles_left_row)
+        sorted_keys = sorted(self.tile_bag)
+        counts = sorted(set([count for [count, _] in
+                             self.tile_bag.values()]), reverse=True)
+        tiles_left = {}
+        for count in counts:
+            tiles = []
+            for tile in sorted_keys:
+                if self.tile_bag[tile][0] == count:
+                    tiles.append(tile)
+            tiles_left[count] = tiles
         return tiles_left
 
-    def take_tiles(self, tile_set=None, num=0):
-        """Take specific set or number of random tiles from the tiles_bag.
+    def take_tiles_set(self, tile_set=None):
+        """Take specific set of tiles from the tiles_bag.
 
         Args:
             tile_set (str or list): set of predefined tiles.
-            num (int): get that amount of random tiles or first num
-                tiles from tile_set if tile_set is not None.
         Returns:
             player_tiles (list): list of tiles taken from the tile_bag.
         """
         player_tiles = []
-        if tile_set:
-            # ensured that requested tiles in the upper case
-            # to properly access tiles in tile_bag 
-            tile_set = tile_set.upper()
-            if not num:
-                num = len(tile_set)
-        else:
-            if num <= len(self._tiles_string):
-                tile_set = random.sample(self._tiles_string, num)
-            else:
-                print("There is less then %d in the bag" % num)
-                return
-        for tile in tile_set[0:num]:
+        # ensured that requested tiles in the upper case
+        tile_set = tile_set.upper()
+
+        for tile in tile_set:
             tile_removed = self._take_one_tile(tile)
             if not tile_removed:
                 break
             player_tiles.append(tile)
         return player_tiles
 
+    def take_tiles_num(self, num=0):
+        """Take specific number of random tiles from the tiles_bag.
 
+        Args:
+            num (int): get that amount of random tiles from the tiles_bag.
+        Returns:
+            player_tiles (list): list of tiles taken from the tile_bag.
+        """
+        player_tiles = []
+        if num <= len(self._tiles_string):
+            tile_set = random.sample(self._tiles_string, num)
+        else:
+            print("There is less then %d in the bag" % num)
+            return
+        for tile in tile_set:
+            tile_removed = self._take_one_tile(tile)
+            if not tile_removed:
+                break
+            player_tiles.append(tile)
+        return player_tiles
 
 if __name__ == '__main__':
-    pass
+    bag = Scrubble()
+    bag.take_tiles_set('AXHDRUIOR_XHJZUQEE')
+    tiles_in_bag = bag.get_tiles_left()
+    
+    for c in sorted(tiles_in_bag, reverse=True):
+        print(c, tiles_in_bag[c])
---
+++ easy/dp_273_easy.py
@@ -0,0 +1,5 @@
+"""
+[2016-06-27] Challenge #273 [Easy] Getting a degree
+https://tinyurl.com/dp-273-easy
+"""
+
