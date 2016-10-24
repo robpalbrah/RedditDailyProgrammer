4ac0028b07491bd707cb13ce391fa9e24e1b42f3
RobPalBrah <robpalbrah@gmail.com>
2016-10-24 08:13:47 +0400

    kaprekar function worcs for 4-digit numbers

--- easy/dp_287_easy.py
+++ easy/dp_287_easy.py
@@ -44,16 +44,16 @@ def num_of_digits_max(input_number, max_digits=4):
 
 
 def ordered_digits(input_number, max_digits=4, descending=False):
-    """Returns an integer with  its' digits ordered. 
-    
+    """Returns an integer with  its' digits ordered.
+
     Args:
         input_number (int): input number
         max_digits (int): input_number will be processed as a number
             with max_digits number of digits. Defaults to 4.
         descending (bool): if True returned integer will have its' digits
-            in descending order, if False - in ascending. Defaults to False. 
+            in descending order, if False - in ascending. Defaults to False.
     Returns:
-        number (int): an integer with ordered digits 
+        number (int): an integer with ordered digits
     """
     adjusted_number = num_of_digits_max(input_number, max_digits=max_digits)
 
@@ -62,77 +62,51 @@ def ordered_digits(input_number, max_digits=4, descending=False):
     return number
 
 def desc_digits(input_number, max_digits=4):
-    """Returns an integer with  its' digits in a descending order. 
-    
+    """Returns an integer with  its' digits in a descending order.
+
     Args:
         input_number (int): input number
         max_digits (int): input_number will be processed as a number
-            with max_digits number of digits. Defaults to 4. 
+            with max_digits number of digits. Defaults to 4.
     Returns:
-        number (int): an integer with ordered digits 
+        number (int): an integer with ordered digits
     """
     return ordered_digits(input_number, descending=True,
                           max_digits=max_digits)
 
 def asc_digits(input_number, max_digits=4):
-    """Returns an integer with  its' digits in a descending order. 
-    
+    """Returns an integer with  its' digits in a descending order.
+
     Args:
         input_number (int): input number
         max_digits (int): input_number will be processed as a number
-            with max_digits number of digits. Defaults to 4. 
+            with max_digits number of digits. Defaults to 4.
     Returns:
-        number (int): an integer with ordered digits 
+        number (int): an integer with ordered digits
     """
     return ordered_digits(input_number, descending=False,
                           max_digits=max_digits)
 
-def diff_digits(input_number, max_digits=4):
-    """Checks whether input_number has two or more different digits.
-    
-    Args:
-        input_number (int): input number
-        max_digits (int): input_number will be processed as a number
-            with max_digits number of digits. Defaults to 4.
-    Returns:
-        bool: False if all digits are the same, True of digits are different. 
-    """
-    input_number = num_of_digits_max(input_number, max_digits=max_digits)
 
-    num_of_diff_digits = len(set(list(str(input_number))))
-    if num_of_diff_digits < 2:
-        return False
-    else:
-        return True
-
-def kaprekar(input_number, iterations=0, prev_number=None, max_digits=4):
+def kaprekar(input_number, iterations=0, max_digits=4):
     """Finds a Kaprekars fixed point in a number with max_digits or less.
 
     Args:
-        input_number (int): input number  
+        input_number (int): input number
         iterations (int): number of previous iteration of the Kaprekars cycle.
             Service argument. Defaults to 0.
-        prev_number (int): previous number in the Kaprekars cycle. Service argument.
-            Defaults to None.
         max_digits (int): input_number will be processed as a number
             with max_digits number of digits. Defaults to 4.
     Returns:
-        tuple (imput_number (int), iterations (int)):  
+        tuple (imput_number (int), iterations (int)):
     """
-    # infinite loop on cycles 
-    if input_number == prev_number:
-        return (input_number, iterations)
-
-    if not diff_digits(input_number, max_digits=max_digits):
-        return (input_number, iterations)
-    else:
-        subt_result = (desc_digits(input_number, max_digits=max_digits) -
+    # infinite loop on cycles
+    subt_result = (desc_digits(input_number, max_digits=max_digits) -
                       asc_digits(input_number, max_digits=max_digits))
-        return kaprekar(subt_result,
-                        iterations=iterations + 1,
-                        prev_number=input_number,
-                        max_digits=max_digits)
-# nothing here has been tested
+    if subt_result == input_number:
+        return (subt_result, iterations)
+    else:
+        return kaprekar(subt_result, iterations=iterations+1, max_digits=max_digits)
 
 
 if __name__ == "__main__":
@@ -148,7 +122,7 @@ if __name__ == "__main__":
             # input number have number of digits bigger than max_digits
             with self.assertRaises(ValueError):
                 num_of_digits_max(12345, max_digits=4)
-                
+
         def test_desc_digits(self):
             self.assertEqual(desc_digits(1234), 4321)
             self.assertEqual(desc_digits(4040), 4400)
@@ -156,7 +130,7 @@ if __name__ == "__main__":
             # input number have number of digits bigger than max_digits
             with self.assertRaises(ValueError):
                 desc_digits(12345, max_digits=4)
-                
+
         def test_asc_digits(self):
             self.assertEqual(asc_digits(4321), 1234)
             self.assertEqual(asc_digits(4040), 44)
@@ -165,13 +139,9 @@ if __name__ == "__main__":
             with self.assertRaises(ValueError):
                 asc_digits(12345, max_digits=4)
 
-        def test_diff_digits(self):
-            self.assertTrue(diff_digits(1234))
-            self.assertTrue(diff_digits(6, max_digits=3)) # processed as 006
-            self.assertFalse(diff_digits(1111))
-            self.assertFalse(diff_digits(0, max_digits=3)) # processed as 000
-            
         def test_kaprekar(self):
-            self.assertEqual(kaprekar(6589)[1], 2) 
-        
+            self.assertEqual(kaprekar(6589)[1], 2)
+            self.assertEqual(kaprekar(5455)[1], 5)
+            self.assertEqual(kaprekar(6174)[1], 0)
+
     unittest.main()
