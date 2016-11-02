37aaee61c848990b6094672d55e1e274503b78ef
RobPalBrah <robpalbrah@gmail.com>
2016-11-02 06:40:24 +0400

    Added range_kaprekar function. Still works only for base 10

--- easy/dp_290_easy.py
+++ easy/dp_290_easy.py
@@ -2,17 +2,18 @@
 [2016-10-31] Challenge #290 [Easy] Kaprekar Numbers
 https://tinyurl.com/dp-290-easy
 """
+
 # works only for base 10
 def is_kaprekar_number(input_number):
     input_squared = str(input_number ** 2)
     num_of_digits = len(input_squared)
-    
-    # range function starts with 1 since 
+
+    # range function starts with 1 since
     # input_squared is devided into two parts
     for i in range(1, num_of_digits):
         first_part = int(input_squared[:i])
         second_part = int(input_squared[i:])
-        
+
         if first_part == 0 or second_part == 0:
             continue
         else:
@@ -22,7 +23,12 @@ def is_kaprekar_number(input_number):
     else:
         return False
 
+def range_kaprekar(start, end):
+    kaprekar_numbers = []
+    for n in range(start, end+1):
+        if is_kaprekar_number(n):
+            kaprekar_numbers.append(n)
+    return kaprekar_numbers
+
 if __name__ == '__main__':
-    for i in range(101, 9000):
-        if is_kaprekar_number(i):
-            print(i)
+    print(range_kaprekar(101, 9000))
