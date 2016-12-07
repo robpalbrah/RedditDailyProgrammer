1419977474dc3e0b886ae8a41afaf69f3fb38be0
RobPalBrah <robpalbrah@gmail.com>
2016-12-07 05:57:50 +0400

    dp_294_easy bonus 3 added

--- easy/dp_294_easy.py
+++ easy/dp_294_easy.py
@@ -45,8 +45,8 @@ def scrabble(letter_tiles, word):
        bool: True for success, False otherwise.
     """
     wildcard_sign = "?"
-    letter_list = list(letter_tiles)
-    for letter in word:
+    letter_list = list(letter_tiles.lower())
+    for letter in word.lower():
         if letter in letter_list:
             letter_list.remove(letter)
         elif wildcard_sign in letter_list:
@@ -84,11 +84,60 @@ def longest(letter_tiles, words_list = ENABLE1):
     return longest_word
 
 
-def highest(letter_tiles, tile_worth = WORTH_TABLE):
-    pass
+def highest(letter_tiles, words_list = ENABLE1, tile_worth = WORTH_TABLE):
+    """
+    Determines highest-scoring word from words_list that can be constructed
+    using letter_tiles. In case of equal scores returns first word with
+    highest score found.
+
+    Args:
+        letter_tiles (str): string of letters and wildcard_sign signs
+        words_list (file): list of words
+        tile_worth (dict):
+            structured as {"tile": ["number of tiles", "tile worth"], ...}
+    Returns:
+        str: highest_scoring_word
+    """
+    highest_scoring_word = ''
+    highest_score = 0
+
+    with open(words_list) as file:
+        for line in file:
+            line = line.strip()
+            word_score = word_worth(letter_tiles, line)
+            if word_score > highest_score:
+                highest_scoring_word = line
+                highest_score = word_score
+    return highest_scoring_word
+
+def word_worth(letter_tiles, word, tile_worth = WORTH_TABLE):
+    """
+    Calculate worth of a word that can be constructed from
+    the given letter_tiles.
+    Args:
+        letter_tiles (str): string of letters and wildcard_sign signs
+        words_list (file): list of words
+        tile_worth (dict):
+            structured as {"tile": ["number of tiles", "tile worth"], ...}
+    Returns:
+        int: word_score, or 0 if a word can't be constructed from the 
+            given letter_tiles
+    """
+    wildcard_sign = "?"
+    word_score = 0
+    letter_list = list(letter_tiles.upper())
+    for letter in word.upper():
+        if letter in letter_list:
+            letter_list.remove(letter)
+            word_score += tile_worth[letter][1]
+        elif wildcard_sign in letter_list:
+            letter_list.remove(wildcard_sign)
+            word_score += tile_worth[wildcard_sign][1]
+        else:
+            return 0
+    return word_score
         
-        
-        
+
 if __name__ == '__main__':
     import unittest
 
@@ -99,20 +148,25 @@ if __name__ == '__main__':
             self.assertTrue(scrabble("orrpgma", "program"))
             self.assertFalse(scrabble("orppgma", "program"))
 
-        def test_scrabble_bonus_1(self):
+        def test_scrabble_wildcard(self):
             self.assertTrue(scrabble("pizza??", "pizzazz"))
             self.assertFalse(scrabble("piizza?", "pizzazz"))
             self.assertTrue(scrabble("a??????", "program"))
             self.assertFalse(scrabble("b??????", "program"))
 
-        def test_longest_bonus_2(self):
+        def test_longest(self):
             self.assertEqual(longest("dcthoyueorza"), "coauthored")
             self.assertEqual(longest("uruqrnytrois"), "turquois")
             self.assertEqual(longest("rryqeiaegicgeo??"), "greengrocery")
             self.assertEqual(longest("udosjanyuiuebr??"), "subordinately")
             self.assertEqual(longest("vaakojeaietg????????"), "ovolactovegetarian")
 
-        def test_highest_bonus_3(self):
+        def test_word_score(self):
+            self.assertEqual(word_worth("", ""), 0)
+            self.assertEqual(word_worth("progaaf????", "program"), 8)
+            self.assertEqual(word_worth("abc", "critter"), 0)
+
+        def test_highest(self):
             self.assertEqual(highest("dcthoyueorza"), "zydeco")
             self.assertEqual(highest("uruqrnytrois"), "squinty")
             self.assertEqual(highest("rryqeiaegicgeo??"), "reacquiring")
